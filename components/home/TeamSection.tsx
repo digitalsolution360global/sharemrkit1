b87adc56862e7962ab606533a6398433c395ec2f
digitalsolution360global <ds360host@gmail.com>
2026-02-08 08:28:03 +0530

    done

--- components/home/TeamSection.tsx
+++ components/home/TeamSection.tsx
@@ -41,7 +41,7 @@ export default function TeamSection() {
         </div>
 
         {/* Team Cards */}
-        <div className="grid grid-cols-1 md:grid-cols-2 gap-10">
+        <div className="grid grid-cols-1 md:grid-cols-3 gap-10">
           {teamMembers.map((member, index) => (
             <Link
               key={index}
